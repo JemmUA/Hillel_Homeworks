abc09e595668a1081292d349414acd0f650b87f0
Oleksandr Omelyan <jemm.ua@gmail.com>
2022-12-22 18:22:00 +0200

    HW 25 Lesson 29 inter 3

--- src/main/java/lesson29/MainHibernate.java
+++ src/main/java/lesson29/MainHibernate.java
@@ -6,15 +6,15 @@ import lesson29.util.HibernateConfiguration;
 
 public class MainHibernate {
     public static void main(String[] args) {
-//        HibernateConfiguration.getSessionFactory();
+        HibernateConfiguration.getSessionFactory();
 
-        CustomerService customerService = new CustomerService();
-        Customer customer = new Customer();
-        customer.setName("Hellen");
-        customer.setAge(18);
-        customer.setEmail("hellen@mail.ua");
-        customer.setPhone(380951232232L);
-        customer.setAbout("barber");
-        customerService.save(customer);
+//        CustomerService customerService = new CustomerService();
+//        Customer customer = new Customer();
+//        customer.setName("Hellen");
+//        customer.setAge(18);
+//        customer.setEmail("hellen@mail.ua");
+//        customer.setPhone(380951232232L);
+//        customer.setAbout("barber");
+//        customerService.save(customer);
     }
 }
