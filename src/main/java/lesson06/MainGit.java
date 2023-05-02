18829fb143d5df3b974acc4f5b00860979bd34b1
Oleksandr Omelyan <jemm.ua@gmail.com>
2023-05-02 20:07:30 +0300

    + git

--- src/main/java/lesson06/MainGit.java
+++ src/main/java/lesson06/MainGit.java
@@ -8,5 +8,7 @@ public class MainGit {
     //      Create new branch
     //      and move to this
     //      branch:                 git branch -b newbranch
-
+    //      cansel failed marge:    git merge --abort
+    //      restore before
+    //      closing IDEA:           git restore
 }
